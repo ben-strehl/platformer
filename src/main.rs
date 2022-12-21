6ede62c676fee709ec205589104c3b60169250f4
Ben Strehl <benjaminstrehl0@gmail.com>
2022-12-21 17:18:21 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2,0.2,0.2)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: WIDTH,
+                height: HEIGHT,
+                title: "Garbage Platformer".to_string(),
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .run();
+}
