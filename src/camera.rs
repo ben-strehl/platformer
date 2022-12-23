2a235067e70f57f7a52b9aed124ef04b3cdeb194
Ben <92104791+ben+strehl@users.noreply.github.com>
2022-12-23 12:25:39 -0500

    Created modules

---
+++ src/camera.rs
@@ -0,0 +1,18 @@
+pub mod camera;
+
+use bevy::prelude::*;
+use crate::*;
+
+pub struct CameraPlugin;
+
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App){
+        
+    }
+}
+
+fn spawn_camera(commands: &mut Commands) {
+    commands
+        .spawn()
+}
